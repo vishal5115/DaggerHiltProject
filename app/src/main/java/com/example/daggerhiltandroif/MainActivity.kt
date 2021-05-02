c0109ed5da5240136ddacaf7d7cbd2f01087600c
Vishal <garg.vishal5115@gmail.com>
2021-05-02 13:35:14 +0530

    Fragment Scope with Dagger hilt

--- app/src/main/java/com/example/daggerhiltandroif/MainActivity.kt
+++ app/src/main/java/com/example/daggerhiltandroif/MainActivity.kt
@@ -2,24 +2,34 @@ package com.example.daggerhiltandroif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var someClass: SomeClass
+//    @Inject
+//    lateinit var someClass: SomeClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(someClass.doAThing())
+//        println(someClass.doAThing())
     }
 }
 
+@AndroidEntryPoint
+class MyFragment: Fragment(){
 
+    @Inject
+    lateinit var someClass: SomeClass
+
+}
+
+@FragmentScoped
 class SomeClass
 @Inject
 constructor(){
